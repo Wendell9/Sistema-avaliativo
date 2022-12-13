9ed9f348d684a081d51966ea1e898642801f5b18
Wendell <guindaniwendell@gmail.com>
2022-12-13 18:48:18 -0300

    Criado um programa simples para sistema analiativo

---
+++ NotaAluno/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SistemaAvaliativo A;
+            A = new SistemaAvaliativo();
+            Console.Write("Nome do aluno: ");
+            Console.WriteLine("Digite as notas: ");
+            A.N1 = double.Parse(Console.ReadLine());
+            A.N2 = double.Parse(Console.ReadLine());
+            A.N3 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Nota final = {A.NotaFinal()}");
+            A.SituacaoAluno();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ NotaAluno/SistemaAvaliativo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotaAluno
+{
+    internal class SistemaAvaliativo
+    {
+        public string Nome;
+        public double N1, N2, N3;
+        public double NotaFinal()
+        {
+            return N1 + N2 + N3;
+        }
+        public bool Aprovado()
+        {
+            if (NotaFinal()>=60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void SituacaoAluno()
+        {
+            if (NotaFinal()>=60)
+            {
+                Console.WriteLine("Você foi aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+                Console.WriteLine($"Falta {60.0 - NotaFinal()}");
+            }
+        }
+    }
+}
